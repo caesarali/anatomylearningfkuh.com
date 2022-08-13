625cf456bb496ee50a8b032ba97b848842489d29
Caesar Ali L <caesaralilamondo@gmail.com>
2022-08-13 16:44:19 +0800

    initial commit

---
+++ assets/warning.js
@@ -0,0 +1,9 @@
+var warningTitleCSS = 'color:red; font-size:60px; font-weight: bold; -webkit-text-stroke: 1px black;';
+var warningDescCSS = 'font-size: 18px;';
+var info = warningDescCSS + 'color:green; font-weight: bold;';
+
+console.log('%cHai...', warningTitleCSS);
+console.log("%cTolong jangan melakukan sesuatu yang aneh menggunakan fitur ini.", warningDescCSS);
+console.log("");
+console.log('%cDeveloped by %cCaesar Ali %cfrom %cSmartinovasi.', warningDescCSS, info, warningDescCSS, info);
+console.log('%cFind us on https://caesarali.com & https://smartinovasi.com', warningDescCSS);
